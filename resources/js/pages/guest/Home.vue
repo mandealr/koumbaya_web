3e8186d7b3213b6d167ed1730112ff2bfd3cc893
Aristide Mebodo <mebodoaristide@yahoo.fr>
2025-08-13 00:40:46 +0100

    Dev : Fix Design Home Guest 2

--- resources/js/pages/guest/Home.vue
+++ resources/js/pages/guest/Home.vue
@@ -93,7 +93,7 @@
               </div>
 
               <!-- Floating Elements -->
-              <div class="absolute -top-8 -left-8 bg-blue-100 text-blue-600 px-4 py-2 rounded-full text-sm font-medium z-20 animate-float">
+              <div class="absolute inline-flex items-center -top-8 -left-8 bg-blue-100 text-blue-600 px-4 py-2 rounded-full text-sm font-medium z-20 animate-float">
                 <SparklesIcon class="h-4 w-4" /> Nouvelle tombola
               </div>
               <div class="absolute -bottom-4 -right-8 bg-yellow-100 text-yellow-600 px-4 py-2 rounded-full text-sm font-medium z-20 animate-float-delayed">
