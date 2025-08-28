125d8f712ceb434c095dfdba2e0e55d0aab67a6f
Aristide Mebodo <mebodoaristide@yahoo.fr>
2025-08-28 13:26:24 +0100

    Dev : Database Update 21

--- app/Http/Controllers/Api/PaymentController.php
+++ app/Http/Controllers/Api/PaymentController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Payment;
-use App\Models\Payment;
 use App\Models\Order;
 use App\Models\LotteryTicket;
 use App\Models\Lottery;
