84b44482a7c20f4faf5540846935cd022598b971
Aristide Mebodo <mebodoaristide@yahoo.fr>
2025-09-11 03:11:06 +0100

    Dev : Email

--- app/Mail/WelcomeEmail.php
+++ app/Mail/WelcomeEmail.php
@@ -41,7 +41,7 @@ class WelcomeEmail extends Mailable
     public function content(): Content
     {
         return new Content(
-            view: 'emails.welcome',
+            markdown: 'emails.welcome',
         );
     }
 
